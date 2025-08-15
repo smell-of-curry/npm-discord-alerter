8da384e2717b3a2891f4fcf84ef3ed6d31bc6d9b
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2025-08-15 09:14:47 -0400

    Update Logo Path

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const DEFAULT_DESCRIPTION_TEMPLATE = "`npm i {package}@{version}`";
 export const DEFAULT_URL_TEMPLATE = "https://www.npmjs.com/package/{package}";
 export const DEFAULT_USERNAME = "Npm";
 export const DEFAULT_AVATAR_URL =
-  "https://raw.githubusercontent.com/smell-of-curry/npm-discord-alerter/main/images/npm-logo.png";
+  "https://raw.githubusercontent.com/smell-of-curry/npm-discord-alerter/main/public/npm-logo.png";
